8c2794b065769176acd34837727ff75a3b477b93
liza <e.bulgakova1@g.nsu.ru>
2022-01-05 21:02:46 +1000

    try3

--- 2/cmake-build-debug/CTestTestfile.cmake
+++ 2/cmake-build-debug/CTestTestfile.cmake
@@ -4,5 +4,3 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/bulga/engnr-nsu-cpp-2021/2/cmake-build-debug/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
